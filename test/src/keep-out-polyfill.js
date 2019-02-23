f39740fd3660a26f1ba82dded86a852c2f49dfdf
jihong <632565970@qq.com>
2019-02-23 15:17:38 +0800

    调整补偿高度

--- test/src/keep-out-polyfill.js
+++ test/src/keep-out-polyfill.js
@@ -1,83 +1,88 @@
-class KeepOut {
-  constructor() {
-    this.winHeight = window.innerHeight;
-    this.navHeight = screen.availHeight - this.winHeight
-    this.isKeepOut = null
-    this.isbodyHide = null
-    
-    this.keyHeight = 0
-    this.clientY = 0
-    this.init()
-  }
-
-  init() {
-    this.initOrientation()
-    this.initFocus()
-    this.initBlur()
-  }
-  initOrientation() {
-    window.addEventListener("orientationchange", () => {
+(function(){
+  class KeepOut {
+    constructor() {
       this.winHeight = window.innerHeight;
       this.navHeight = screen.availHeight - this.winHeight
-      this.getKeiHeight()
-    });
-  }
-
-  isResize() {
-    // 配合keyboard事件使用
-    let curHeight = window.innerHeight;
-    if (Math.abs(curHeight - this.winHeight) > this.navHeight) {
-      return true
+      this.isKeepOut = null
+      this.isbodyHide = null
+      this.dispatchResize = false
+      
+      this.keyHeight = 0
+      this.clientY = 0
+      this.init()
     }
-    return false
-  }
-
-  getKeiHeight () {
-    if(this.isKeepOut){
-      this.keyHeight = screen.availHeight - window.innerHeight - 50
+  
+    init() {
+      this.initOrientation()
+      this.initFocus()
+      this.initBlur()
     }
-  }
-
-  onceComput () {
-    if(this.isKeepOut === null){
-      this.isKeepOut = this.isResize()
-      this.getKeiHeight()
+    initOrientation() {
+      window.addEventListener("orientationchange", () => {
+        this.winHeight = window.innerHeight;
+        this.navHeight = screen.availHeight - this.winHeight
+        this.dispatchResize = false
+      });
     }
-    this.onceComput = function(){}
-  }
-
-  focusFn() {
-    if(this.isKeepOut === false || this.isbodyHide){
-      if(this.clientY > this.keyHeight) {
-        let polyfill = this.clientY - this.keyHeight
-        document.body.style.transition = '0.2s'
-        document.body.style.transform = `translateY(-${polyfill}px)`
+  
+    isResize() {
+      // 配合keyboard事件使用
+      let curHeight = window.innerHeight;
+      if (Math.abs(curHeight - this.winHeight) > this.navHeight) {
+        return true
+      }
+      return false
+    }
+  
+    getKeiHeight () {
+      if(this.isKeepOut){
+        this.keyHeight = screen.availHeight - window.innerHeight - 50
+      }
+      else{
+        this.keyHeight = window.innerHeight / 2
       }
     }
-  }
-
-  blurFn() {
-    if(this.isKeepOut === false || this.isbodyHide){
+  
+    onceComput () {
+      if(this.dispatchResize) return;
+      this.dispatchResize = true
+      if(this.isKeepOut === null){
+        this.isKeepOut = this.isResize()
+        this.getKeiHeight()
+      }
+    }
+  
+    focusFn() {
+      if(this.clientY > this.keyHeight) {
+        if(this.isKeepOut === false || this.isbodyHide){
+          let polyfill = this.clientY - this.keyHeight
+            document.body.style.transition = '0.2s'
+            document.body.style.transform = `translateY(-${polyfill}px)`
+        }
+      }
+    }
+  
+    blurFn() {
       document.body.style.transform = `translateY(0px)`
     }
+  
+    initFocus() {
+      window.addEventListener('click', (e) => {
+        let event = e || window.event
+        this.clientY = event.clientY
+      })
+      window.addEventListener('keyboardFocus', () => {
+        this.isbodyHide = window.getComputedStyle(document.body, null).getPropertyValue("overflow") === 'hidden'
+        this.onceComput()
+        this.focusFn()
+      })
+    }
+  
+    initBlur() {
+      window.addEventListener('keyboardBlur', () => {
+        this.blurFn()
+      })
+    }
   }
-
-  initFocus() {
-    window.addEventListener('click', (e) => {
-      let event = e || window.event
-      this.clientY = event.clientY
-    })
-    window.addEventListener('keyboardFocus', () => {
-      this.isbodyHide = window.getComputedStyle(document.body, null).getPropertyValue("overflow") === 'hidden'
-      this.onceComput()
-      this.focusFn()
-    })
-  }
-
-  initBlur() {
-    window.addEventListener('keyboardBlur', () => {
-      this.blurFn()
-    })
-  }
-}
-new KeepOut()
+  new KeepOut()
+})()
